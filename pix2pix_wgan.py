c0a9f6b35d1cfdd9b60beb4bc64fa5de258261e4
FengyangZhang <fengyangzhang@d-172-25-98-112.dhcp.virginia.edu>
2017-11-25 11:11:26 -0500

    modified wgan D loss

--- pix2pix_wgan.py
+++ pix2pix_wgan.py
@@ -460,7 +460,7 @@ def create_model(inputs, targets):
         # minimizing -tf.log will try to get inputs to 1
         # predict_real => 1
         # predict_fake => 0
-        discrim_loss = tf.reduce_mean(predict_real) - tf.reduce_mean(predict_fake)
+        discrim_loss = - tf.reduce_mean(predict_real) + tf.reduce_mean(predict_fake)
 
     with tf.name_scope("generator_loss"):
         # predict_fake => 1
